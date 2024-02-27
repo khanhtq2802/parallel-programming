aeb9df22e9e22ca62ca030abb326cf1c9a15f86e
khanh.tq2802 <khanh.tq2802@outlook.com>
2024-02-28 06:42:50 +0700

    upload files

--- hello.cu
+++ hello.cu
@@ -1,6 +1,22 @@
 #include <stdio.h>
-#include <cuda_runtime_api.h>
-int main(void){
-    printf("Hello, World!\n");
+
+#define CUDA_CHECK_ERROR() \
+do { \
+    cudaError_t err = cudaGetLastError(); \
+    if (err != cudaSuccess) { \
+        printf("CUDA error: %s, line %d\n", cudaGetErrorString(err), __LINE__); \
+        exit(EXIT_FAILURE); \
+    } \
+} while (0)
+
+__global__ void helloCUDA() {
+    printf("Hello CUDA from GPU!\n");
+}
+
+int main() {
+    helloCUDA<<<1, 1>>>();
+    CUDA_CHECK_ERROR();
+    cudaDeviceSynchronize();
+    CUDA_CHECK_ERROR();
     return 0;
 }
